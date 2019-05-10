ea355fa691af28c47d711a6dbb88f6ace81a16ef
Peter Walter Tafirenyika Chifamba <PeterWalter@users.noreply.github.com>
2019-05-10 16:47:53 +0200

    master detail
    made t create a proper master detail table

--- flamelily.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ flamelily.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "92ec8ce3-4b60-4bfd-88f0-f4ed5eaa5629",
+		"XAMARIN_BUILD_ID", "81c6f0aa-7678-4e25-888b-099a14ab76fb",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep",
